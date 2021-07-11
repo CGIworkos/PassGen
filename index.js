5038396d0b3dc28faf15068c7c7956fe0c0fe777
Aleksandr Osipenko <archivizer.workos@gmail.com>
2021-07-11 16:58:38 +0300

    Config utils file for save as txt file generating pass

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const chalk = require('chalk')
 const clipboardy = require('clipboardy')
 const log = console.log
 const createPassword = require('./utils/createPassword')
+const savePassword = require('./utils/savePassword')
 
 program.version('1.0.0').description('Simple Password Generator')
 
 program
-    .option('-l, --length <number>', 'length of password', '8')
+    .option('-l, --length <number>', 'length of password', '10')
     .option('-s, --save', 'save password to password.txt')
     .option('-nn, --no-numbers', 'remove numbers')
     .option('-ns, --no-symbols', 'remove symbols')
@@ -16,6 +17,10 @@ program
 const { length, save, numbers, symbols } = program.opts()
 
 const generatedPassword = createPassword(length, numbers, symbols)
+
+if (save) {
+    savePassword(generatedPassword)
+}
 
 clipboardy.writeSync(generatedPassword)
 
