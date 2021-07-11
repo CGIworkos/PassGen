b9644c7d96373cb6984bdb90a044d2e0155aa53e
Aleksandr Osipenko <archivizer.workos@gmail.com>
2021-07-11 16:43:04 +0300

    Adding shortcodes for app

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-if (process.argv[2] === 'generate') {
-    console.log('Generated')
-}
+const program = require('commander')
+const log = console.log
+const createPassword = require('./utils/createPassword')
+
+program.version('1.0.0').description('Simple Password Generator')
+
+program
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'save password to password.txt')
+    .option('-nn, --no-numbers', 'remove numbers')
+    .option('-ns, --no-symbols', 'remove symbols')
+    .parse()
+
+const { length, save, numbers, symbols } = program.opts()
+
+const generatedPassword = createPassword(length, numbers, symbols)
+
+log(generatedPassword)
